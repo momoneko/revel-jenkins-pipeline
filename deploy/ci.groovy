7d269774555ed6903611a61861bcd10a69f7ad54
Dumitru Melenteanu <dumitru.melentaenu@yopeso.com>
2017-01-16 16:30:18 +0200

    List files

--- deploy/ci.groovy
+++ deploy/ci.groovy
@@ -14,6 +14,7 @@ def runProvisioning() {
     // sh 'cp ./conf/settings.conf.sample ./conf/settings.conf'
     sh 'echo $PATH'
     sh 'echo $(pwd)'
+    sh 'ls'
    withEnv(["GOPATH=~/go-workspace"]) {
         withEnv(["PATH=PATH:/usr/local/bin/"]) {
                 sh 'glide install'
