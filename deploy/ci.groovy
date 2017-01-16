ecb84a4849e3b1c305d459762f536589efda9e44
Dumitru Melenteanu <dumitru.melentaenu@yopeso.com>
2017-01-16 16:26:37 +0200

    Set gopath

--- deploy/ci.groovy
+++ deploy/ci.groovy
@@ -14,6 +14,9 @@ def runProvisioning() {
     // sh 'cp ./conf/settings.conf.sample ./conf/settings.conf'
     sh 'echo $PATH'
     sh 'echo $(pwd)'
+   withEnv(["GOPATH=~/go-workspace"]) {
+        sh 'glide install'
+    }
    withEnv(["PATH=PATH:/usr/local/bin/"]) {
         sh 'glide install'
     }
