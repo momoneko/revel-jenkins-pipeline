2f54f94348b49ab49062708d98b19c97fdd00fd3
Dumitru Melenteanu <dumitru.melentaenu@yopeso.com>
2017-01-16 17:09:13 +0200

    Groovy complains

--- deploy/ci.groovy
+++ deploy/ci.groovy
@@ -15,7 +15,7 @@ def runProvisioning() {
     sh 'echo $PATH'
     sh 'echo $(pwd)'
     sh 'ls'
-   withEnv(["GOPATH=$(pwd)/go-workspace"]) {
+   withEnv(['GOPATH=$(pwd)/go-workspace']) {
         // -p so that mkdir does not whine when folders exists
         sh 'mkdir -p $GOPATH'
         sh 'ls | grep -v $(basename $GOPATH) | xargs echo '
